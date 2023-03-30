ff1bd9d0627edc7240616728085a07ee1272a225
khurramsagoo <uaf.khurram@gmail.com>
2023-03-30 05:08:55 -0700

    git init

---
+++ script.js
@@ -0,0 +1,26 @@
+function sayHello (){
+    console.log("hello world")
+}
+sayHello();
+
+
+
+const sayBye =
+()=>{
+    console.log("sayBye bye")
+}
+sayBye()
+
+
+const name="khurram";
+const age=30;
+console.log(name+ "is "  +age+ "years old");
+
+
+// function script  () {
+//   return (
+//     <div>script</div>
+//   )
+// }
+
+// export default script
